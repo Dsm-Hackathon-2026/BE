8cbc937883f59faed1ef9b0f327611a61627b13e
김지윤 <jjunkim@dsm.hs.kr>
2026-07-13 15:12:17 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/BE/BEApplication.kt
@@ -0,0 +1,11 @@
+package com.example.BE
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BEApplication
+
+fun main(args: Array<String>) {
+    runApplication<BEApplication>(*args)
+}
